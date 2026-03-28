33f4279cf51ea7eebf376bb70022e87c78996fb0
dpcastagna <daniele.castagna@outlook.com>
2026-03-28 21:28:40 +0200

    Initial commit

---
+++ koodi.js
@@ -0,0 +1,5 @@
+'use strict';
+console.log('I have awaken!');
+
+const name = 'Frank';
+document.querySelector('#teksti').innerHTML = 'Good morning, ' + name + '!';
---
+++ module1/assignment02/koodi.js
@@ -0,0 +1,5 @@
+'use strict';
+console.log('I have awaken!');
+
+const name = prompt("Anna nimesi:");
+document.querySelector('#teksti').innerHTML = 'Hello, ' + name + '!';
