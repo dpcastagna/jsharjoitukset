b1c878324118df2b73afa5b2da6b0f867609f421
dpcastagna <daniele.castagna@outlook.com>
2026-04-06 13:14:26 +0300

    2.10 tehty

--- module2/assignment10/koodi.js
+++ module2/assignment10/koodi.js
@@ -1,7 +1,6 @@
 //Äänestysohjelma
 'use strict';
 
-
 const ehdokkaat = [];
 
 let määrä = +prompt("Montako ehdokasta?");
@@ -14,6 +13,7 @@ for (let i = 1; i < määrä + 1; i++) {
 let äänet = +prompt("Montako äänestäjää?")
 
 for (let i = 0; i < äänet; i++) {
+  //console.log(ehdokkaat);
   let ääni = prompt(`Äänestäjä ${i + 1}, ketä äänestät?`);
   for (let j = 0; j < ehdokkaat.length; j++) {
     if (ääni === ehdokkaat[j].nimi) {
@@ -22,14 +22,13 @@ for (let i = 0; i < äänet; i++) {
     }
   }
 }
-console.log(ehdokkaat);
-ehdokkaat.sort((a,b) =>  a.votes + b.votes)
-console.log(ehdokkaat);
-console.log(`Voittaja on ${ehdokkaat[0].nimi} ${ehdokkaat[0].votes}:llä äänellä.`);
+//console.log(ehdokkaat);
+ehdokkaat.sort((a, b) =>  a.votes - b.votes);
+//console.log(ehdokkaat);
+console.log(`Voittaja on ${ehdokkaat[ehdokkaat.length - 1].nimi} ${ehdokkaat[ehdokkaat.length - 1].votes}:llä äänellä.`);
 
-for (let j = 0; j < ehdokkaat.length; j++) {
-  console.log(`${ehdokkaat[0].nimi}: ${ehdokkaat[0].votes} ääntä.`);
-  console.log(j)
+for (let i = ehdokkaat.length - 1; i >= 0; i--) {
+  console.log(`${ehdokkaat[i].nimi}: ${ehdokkaat[i].votes} ääntä.`);
 }
 
 //document.querySelector('#teksti').innerHTML = vastaus;
