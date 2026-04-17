20c83a9052cf4ffae924da169ee6c31b76c825ce
dpcastagna <daniele.castagna@outlook.com>
2026-04-17 12:05:00 +0300

    4.6 tehty 4.5 poistettu turha kommentti

--- module4/assignment06/koodi.js
+++ module4/assignment06/koodi.js
@@ -1,47 +1,24 @@
 //haetaan chuck norris vitsi ja tulostetaan se konsoliin
 'use strict';
 
-async function haeVitsi() {
-    const response = await fetch('https://api.chucknorris.io/jokes/random')
-    const jsonData = await response.json();
-    console.log(jsonData.value);
-}
-
-haeVitsi();
-
-const seriesForm = document.querySelector('#series-form')
-seriesForm.addEventListener('submit', async function(evt) {
+const jokeForm = document.querySelector('#joke-form')
+jokeForm.addEventListener('submit', async function(evt) {
     // ... prevent the default action.
     evt.preventDefault();
     // get value of input element
     const code = document.querySelector('input[name=q]').value;
     const tulos = document.querySelector('#results')
     try {
-        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${code}`);
+        const response = await fetch(`https://api.chucknorris.io/jokes/search?query=${code}`);
         const jsonData = await response.json();
         tulos.innerHTML = '';
         console.log(jsonData);
-        for (let show of jsonData) {
+        for (let joke of jsonData.result) {
             const article = document.createElement('article');
 
-            const otsikko = document.createElement('h1');
-            otsikko.textContent = show.show.name;
-            article.appendChild(otsikko);
-
-            const linkki = document.createElement('a');
-            linkki.href = show.show.url;
-            linkki.target = "_blank";
-            linkki.textContent = show.show.url;
-            article.appendChild(linkki);
-
-            const kuva = document.createElement('img');
-            kuva.src = show.show.image == null ? 'https://placehold.co/210x295?text=Not%20Found' : show.show.image.medium;
-            kuva.alt = show.show.name;
-            article.appendChild(kuva);
-
-            const kuvaus = document.createElement('div');
-            kuvaus.innerHTML = show.show.summary;
-            article.appendChild(kuvaus);
+            const vitsi = document.createElement('p');
+            vitsi.textContent = joke.value;
+            article.appendChild(vitsi);
 
             tulos.appendChild(article);
         }
